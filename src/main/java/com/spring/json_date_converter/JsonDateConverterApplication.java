88333560a336523724692416f18a4d408f06ba61
Vetrivel <vetrivel@prodian.co.in>
2025-03-19 23:22:08 +0530

    initua cimmit

---
+++ src/main/java/com/spring/json_date_converter/JsonDateConverterApplication.java
@@ -0,0 +1,13 @@
+package com.spring.json_date_converter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonDateConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonDateConverterApplication.class, args);
+	}
+
+}
